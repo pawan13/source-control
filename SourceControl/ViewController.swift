bd6023ea0b717cfb0adb481b7d8ee9f764c5bacd
pawansiwakoti <pawanshiwakoti13gmail.com>
2020-06-04 11:03:02 +1000

    Initial Commit

---
+++ SourceControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SourceControl
+//
+//  Created by Pawan  Siwakoti on 4/6/20.
+//  Copyright © 2020 Pawan  Siwakoti. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
